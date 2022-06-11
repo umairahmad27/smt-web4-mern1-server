a662a32d55aedfd3f9ece78e1bedc0acb17b3668
Muhammad Umair Ahmad <m.umairahmad1@gmail.com>
2022-06-11 10:57:01 +0500

    CRUD operaton completed

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ app.use(cors())
 
 mongoose.connect("mongodb+srv://root:root@cluster0.lknvzlq.mongodb.net/ecommerce?retryWrites=true&w=majority")
 
+app.post("/createUser", async (req, res) => {
+    const user = req.body;
+    const newUser = new UserModel(user);
+    await newUser.save()
+
+    res.json(user)
+})
+
 app.get("/getUsers", (request, response) => {
     UserModel.find({}, (err, result) => {
         if (!err) {
@@ -18,13 +26,32 @@ app.get("/getUsers", (request, response) => {
         }
     })
 })
-app.post("/createUser", async (req, res) => {
-    const user = req.body;
-    const newUser = new UserModel(user);
-    await newUser.save()
 
-    res.json(user)
+app.put("/updateUser", (req, res) => {
+
+    const { id, name, age } = req.body
+
+    try {
+        UserModel.findById(id, (err, user) => {
+            console.log(user)
+            user.name = name
+            user.age = age
+            user.save()
+            res.send("User has been successfully updated in DB")
+        })
+    }
+    catch (err) {
+        res.send("Getting error from server")
+    }
 })
+
+app.delete("/deleteUser/:id", async (req, res) => {
+    const id = req.params.id
+
+    await UserModel.findByIdAndRemove(id).exec()
+    res.send("User has been successfully deleted from DB")
+})
+
 
 const PORT = "8000"
 app.listen(PORT, () => {
